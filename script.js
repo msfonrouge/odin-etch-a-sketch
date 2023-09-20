f8a5fcd5e44d936c76e39a43ba105c72194c6ef1
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-09-20 18:57:02 -0300

    Add grid size change and some style

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement("div");
-    container.appendChild(div);
-    for (let j = 0; j < 16; j++) {
-        let div2 = document.createElement("div");
-        div.appendChild(div2);
-    }
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
+
+function generateGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    let div = document.createElement("div");
+    div.classList.add("row");
+    container.appendChild(div);
+    for (let j = 0; j < gridSize; j++) {
+      let div2 = document.createElement("div");
+      div.appendChild(div2);
+      div2.classList.add("square");
+    }
+  }
+}
+
+function colorSquares() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = `rgb(${getRandomInt(256)}, ${getRandomInt(
+        256
+      )}, ${getRandomInt(256)})`;
+    });
+  });
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => container.removeChild(row));
+    generateGrid(prompt("What size?"));
+    colorSquares();
+});
+
+generateGrid(16);
+colorSquares();
+
 
